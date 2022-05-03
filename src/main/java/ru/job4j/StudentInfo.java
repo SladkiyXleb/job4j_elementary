446c948ffd0bb77ff80aa5f15a589ffc551c4022
Sladkiyxleb <sladkiyxleb1@bk.ru>
2022-05-04 01:06:07 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Dmitriy Dmitrievich");
+        System.out.println("02.06.1997");
+    }
+}
